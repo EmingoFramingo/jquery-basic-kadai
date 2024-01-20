baca3b0a8f52d97229cbcd534757da247fc3d678
EmingoFramingo <152578769+EmingoFramingo@users.noreply.github.com>
2024-01-20 09:19:39 +0900

    upload kadai_004

---
+++ kadai_004/event.js
@@ -0,0 +1,9 @@
+$(function() {
+    $(window).on('load', (e) => {
+        console.log('loadイベントが発生しました');
+    });
+
+    $(window).on('scroll', (e) => {
+        console.log('scrollイベントが発生しました');
+    });
+});
